b4e5314257f19c0cc326525cfbffbefd9e733fc7
Nadia <34932@gcslahore.du.pk>
2023-10-20 23:50:59 +0500

    modified design

--- src/pages/project_details/ProjectDetials.jsx
+++ src/pages/project_details/ProjectDetials.jsx
@@ -2,14 +2,31 @@ import React from 'react'
 import './ProjectDetails.css'
 import { useParams } from 'react-router-dom'
 import projectsArray from '../../assets/data/projects'
+import {AiOutlineArrowLeft} from 'react-icons/ai'
 const ProjectDetials = () => {
   const params = useParams()
   const id=params.id
   const project= projectsArray.find(project=>project.id==id)
 
+
   
   return (
 <div className="project_details">
+    <a 
+    style={
+      {
+        alignSelf:'start',
+        color:'black',
+        marginLeft:'10rem',
+        fontSize:'1.5rem'
+      }
+    }
+     href="/" className="back">
+      <AiOutlineArrowLeft/>
+      &nbsp;
+      <span>Back</span>
+    </a>
+
     <h1>{project.name}</h1>
     <div className="project_details_image">
     {
